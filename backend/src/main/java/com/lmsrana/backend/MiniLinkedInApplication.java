3fa103e5630fb5a137bd3c616efdc00d8b9fc10c
Faycal <faycallahri999@gmail.com>
2026-03-18 10:40:18 +0000

    Add backend folder

---
+++ backend/src/main/java/com/lmsrana/backend/MiniLinkedInApplication.java
@@ -0,0 +1,13 @@
+package com.lmsrana.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniLinkedInApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniLinkedInApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/lmsrana/backend/MiniLinkedInApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lmsrana.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniLinkedInApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
